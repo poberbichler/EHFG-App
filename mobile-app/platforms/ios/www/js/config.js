746af5c16c98c7c1a898af1c04272995a9f7c119
Patrick Oberbichler <patrick.oberbichler@gmx.at>
2014-05-04 18:17:23 +0200

    added ios config file

---
+++ mobile-app/platforms/ios/www/js/config.js
@@ -0,0 +1,7 @@
+/**
+ * constant where the server url is
+ * @type {string}
+ */
+//var SERVER_URL = "http://192.168.0.11:8080";
+var SERVER_URL = "http://localhost:8080";
+//var SERVER_URL = "http://app-ehfg.rhcloud.com/app-web";
