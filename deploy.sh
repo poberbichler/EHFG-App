c4ed244f56ab64465134bcb8c347bf45809aacc0
Patrick Oberbichler <patrick.oberbichler@gmx.at>
2014-07-27 16:39:21 +0200

    updated deployment, so the right config will get deployed

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ readonly PARENT=~/git-repository/app-parent
 readonly TARGET=~/git-repository/tomcat-ehfg/webapps
 
 cd $PARENT
-mvn clean package -DskipTests
+mvn clean package -DskipTests -P server
 mv "app-web/target/app-web-0.0.1-SNAPSHOT.war" "$TARGET/app-web.war"
 cd $TARGET
 git add app-web.war
