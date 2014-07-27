62317494cbe7267780e35c9d5a49418b980d44f8
Patrick Oberbichler <patrick.oberbichler@gmx.at>
2014-07-27 15:27:37 +0200

    added url handling for twitter ffeed

--- app-external/src/main/java/org/ehfg/app/external/twitter/TweetFactory.java
+++ app-external/src/main/java/org/ehfg/app/external/twitter/TweetFactory.java
@@ -1,9 +1,14 @@
 package org.ehfg.app.external.twitter;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.ehfg.app.core.entities.Tweet;
 import org.ehfg.app.core.entities.TwitterUser;
 
 import twitter4j.Status;
+import twitter4j.URLEntity;
 
 /**
  * @author patrick
@@ -17,17 +22,42 @@ public class TweetFactory {
 		result.setHashtag(hashtag);
 		result.setId(status.getId());
 		result.setMessage(status.getText());
-		
+
+		final Map<String, URLEntity> urlMap = createUrlMap(status.getURLEntities());
+
 		final StringBuilder builder = new StringBuilder();
 		for (String split : status.getText().split(" ")) {
 			if (split.startsWith("#")) {
 				split = split.replace("#", "<span class=\"hashtag\">#").concat("</span>");
 			}
-			
+
+			else if (urlMap.containsKey(split)) {
+				URLEntity urlEntity = urlMap.get(split);
+				split = String.format("<a href=\"%s\" target=\"_blank\">%s</a>", urlEntity.getExpandedURL(), urlEntity.getDisplayURL());
+			}
+
 			builder.append(split).append(" ");
 		}
-		
+
 		result.setFormattedMesssage(builder.toString());
+		return result;
+	}
+
+	/**
+	 * Creates a map for the given array of {@link URLEntity}.<br>
+	 * The key of the map will be the <strong>displaytext</strong> of the url,
+	 * where the value will be the full {@link URLEntity}
+	 */
+	private static Map<String, URLEntity> createUrlMap(final URLEntity[] urlEntities) {
+		if (urlEntities == null || urlEntities.length == 0) {
+			return Collections.emptyMap();
+		}
+
+		final Map<String, URLEntity> result = new HashMap<>();
+		for (final URLEntity urlEntity : urlEntities) {
+			result.put(urlEntity.getURL(), urlEntity);
+		}
+
 		return result;
 	}
 }
---
+++ app-external/src/test/java/org/ehfg/app/external/twitter/TweetFactoryTest.java
@@ -0,0 +1,56 @@
+package org.ehfg.app.external.twitter;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+
+import org.ehfg.app.core.entities.Tweet;
+import org.ehfg.app.core.entities.TwitterUser;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import twitter4j.Status;
+import twitter4j.URLEntity;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TweetFactoryTest {
+	@Mock
+	private Status status;
+
+	@Mock
+	private TwitterUser user;
+
+	private static final String ANY_HASHTAG = "#EHFG2014";
+	private static final long ANY_ID = 1235234908L;
+	private static final String MESSAGE = "OMG, #EHFG2014 was awsome... #EHFG2013; http://t.co/IyExg1bi61 app.ehfg.org/1234 app.ehfg.org/12";
+
+	@Before
+	public void initMocks() {
+		URLEntity entity = mock(URLEntity.class);
+		when(entity.getDisplayURL()).thenReturn("app.ehfg.org/123");
+		when(entity.getExpandedURL()).thenReturn("https://app.ehfg.org/123");
+		when(entity.getURL()).thenReturn("http://t.co/IyExg1bi61");
+
+		when(status.getCreatedAt()).thenReturn(new Date());
+		when(status.getId()).thenReturn(ANY_ID);
+		when(status.getText()).thenReturn(MESSAGE);
+		when(status.getURLEntities()).thenReturn(new URLEntity[] { entity });
+	}
+
+	@Test
+	public void shouldUpdateUrlsCorrect() {
+		Tweet createdTweet = TweetFactory.create(status, ANY_HASHTAG, user);
+		assertEquals(user, createdTweet.getAuthor());
+		assertEquals(MESSAGE, createdTweet.getMessage());
+		assertEquals(ANY_HASHTAG, createdTweet.getHashtag());
+		final String expectedFormattedMessage = "OMG, <span class=\"hashtag\">#EHFG2014</span> was awsome... <span class=\"hashtag\">#EHFG2013;</span> "
+				+ "<a href=\"https://app.ehfg.org/123\" target=\"_blank\">app.ehfg.org/123</a> app.ehfg.org/1234 app.ehfg.org/12 ";
+
+		assertEquals(expectedFormattedMessage, createdTweet.getFormattedMesssage());
+	}
+}
