d893e28cd56ee72544e82e345f5badda1a611816
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2015-05-24 10:25:45 +0200

    fixed corsfilter

--- app-web/src/main/java/org/ehfg/app/config/SimpleCorsFilter.java
+++ app-web/src/main/java/org/ehfg/app/config/SimpleCorsFilter.java
@@ -7,18 +7,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
  * @author patrick
  * @since 04.2015
  */
+@Component
 class SimpleCorsFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.addHeader("Access-Control-Max-Age", "3600");
+		response.addHeader("Access-Control-Allow-Headers", "x-requested-with");
+
+		filterChain.doFilter(request, response);
 	}
 }
