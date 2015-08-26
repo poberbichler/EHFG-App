2953ea0d6554c0eee371920fd80e76bf10564136
poberbichler <patrick.oberbichler@gmail.com>
2015-08-26 08:46:23 -0800

    fixed tweet timestamp

--- app-core/src/main/java/org/ehfg/app/twitter/TweetMapper.java
+++ app-core/src/main/java/org/ehfg/app/twitter/TweetMapper.java
@@ -30,7 +30,7 @@ final class TweetMapper {
 			final String message = tweet.getFormattedMesssage() != null ? tweet.getFormattedMesssage() : tweet.getMessage();
 
 			// TODO: not an ideal solution, creationDate inside Tweet should be ZoneDateTime next year (?)
-			final ZonedDateTime utcCreationDate = tweet.getCreationDate().atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("Europe/Vienna"));
+			final ZonedDateTime utcCreationDate = tweet.getCreationDate().atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("UTC"));
 			return new TweetDTO(tweet.getId(), user.getFullName(), user.getNickName(), message, user.getProfileImage(), utcCreationDate.toLocalDateTime());
 		}).collect(Collectors.toList());
 	}
