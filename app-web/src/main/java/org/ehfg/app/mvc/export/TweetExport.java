bd4c1299017a213a3d21f9593a00ede012f157ec
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2016-05-29 11:05:02 +0200

    changed deprecated xls export classes

--- app-web/src/main/java/org/ehfg/app/mvc/export/TweetExport.java
+++ app-web/src/main/java/org/ehfg/app/mvc/export/TweetExport.java
@@ -1,13 +1,13 @@
 package org.ehfg.app.mvc.export;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.ehfg.app.twitter.TweetDTO;
 import org.ehfg.app.twitter.TwitterFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.view.document.AbstractExcelView;
+import org.springframework.web.servlet.view.document.AbstractXlsView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,7 +20,7 @@ import java.util.Map;
  * @since 10.2015
  */
 @Controller
-class TweetExport extends AbstractExcelView {
+class TweetExport extends AbstractXlsView {
     private static final int TEXT_COLUMN_INDEX = 0;
     private static final int FULL_NAME_COLUMN_INDEX = 1;
     private static final int NICKNAME_COLUMN_INDEX = 2;
@@ -36,10 +36,9 @@ class TweetExport extends AbstractExcelView {
     }
 
     @Override
-	protected void buildExcelDocument(Map<String, Object> model, HSSFWorkbook workbook, HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-        final HSSFSheet tweetSheet = workbook.createSheet("Tweets");
-        HSSFRow headerRow = tweetSheet.createRow(0);
+    protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        final Sheet tweetSheet = workbook.createSheet("Tweets");
+        Row headerRow = tweetSheet.createRow(0);
 
         headerRow.createCell(TEXT_COLUMN_INDEX).setCellValue("Text");
         headerRow.createCell(FULL_NAME_COLUMN_INDEX).setCellValue("User");
@@ -50,11 +49,12 @@ class TweetExport extends AbstractExcelView {
         for (int rowCounter = 0; rowCounter < tweets.size(); rowCounter++) {
             TweetDTO tweet = tweets.get(rowCounter);
 
-            HSSFRow tweetRow = tweetSheet.createRow(rowCounter+1);
+            Row tweetRow = tweetSheet.createRow(rowCounter+1);
             tweetRow.createCell(TEXT_COLUMN_INDEX).setCellValue(tweet.getMessage());
             tweetRow.createCell(FULL_NAME_COLUMN_INDEX).setCellValue(tweet.getFullName());
             tweetRow.createCell(NICKNAME_COLUMN_INDEX).setCellValue(tweet.getNickName());
             tweetRow.createCell(CREATED_COLUMN_INDEX).setCellValue(tweet.getTimestamp().format(DATE_TIME_FORMATTER));
         }
+
     }
 }
