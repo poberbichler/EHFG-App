e6eba40f4a4539168a1436f33e9e410f2decacc4
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2016-08-28 16:19:41 +0200

    update about text to this years theme

--- mobile-app/www/js/menu.js
+++ mobile-app/www/js/menu.js
@@ -11,7 +11,7 @@
 				template: 
 					'<p>The European Health Forum Gastein is an annual international conference where stakeholders within '
 		            + 'the field of healthcare and public health meet to discuss a broad spectrum of important topics.</p>'
-		            + '<p>The theme of this year is "Securing health in Europe. Balancing priorities, sharing responsibilities"</p>'
+		            + '<p>The theme of this year is "Demographics and Diversity in Europe - New Solutions for Health"</p>'
 		            + '<p>Please direct any questions or concerns to <a href="mailto:info@ehfg.org">info@ehfg.org</a></p>',
 				title: 'About',
 				buttons: [{text: 'Exit'}]
